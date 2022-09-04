cbbb055b5d0c6fde69d95151b224f2dd906fa200
mtufekci1446 <mtufekci1446@gmail.com>
2022-09-05 02:07:49 +0300

    AppService Reg eklendi

---
+++ kodlamaDevs/Application/Features/Languages/Commands/CreateLanguage/CreateLanguageCommand.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.Languages.Dtos;
+using Application.Features.Languages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Languages.Commands.CreateLanguage
+{
+    public class CreateLanguageCommand : IRequest<CreatedLanguageDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateLanguageCommandHandler : IRequestHandler<CreateLanguageCommand, CreatedLanguageDto>
+        {
+            private readonly ILanguageRepository _LanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly LanguageBusinessRules _LanguageBusinessRules;
+
+            public CreateLanguageCommandHandler(ILanguageRepository LanguageRepository, IMapper mapper, LanguageBusinessRules LanguageBusinessRules)
+            {
+                _LanguageRepository = LanguageRepository;
+                _mapper = mapper;
+                _LanguageBusinessRules = LanguageBusinessRules;
+            }
+
+            public async Task<CreatedLanguageDto> Handle(CreateLanguageCommand request, CancellationToken cancellationToken)
+            {
+                await _LanguageBusinessRules.LanguageNameCanNotBeDuplicatedWhenInserted(request.Name);
+
+                Language mappedLanguage = _mapper.Map<Language>(request);
+                Language createdLanguage = await _LanguageRepository.AddAsync(mappedLanguage);
+                CreatedLanguageDto createdLanguageDto = _mapper.Map<CreatedLanguageDto>(createdLanguage);
+
+                return createdLanguageDto;
+            }
+        }
+    }
+}
---
+++ kodlamaDevs/Application/Features/Languages/Rules/LanguageBusinessRules.cs
@@ -0,0 +1,33 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Languages.Rules
+{
+    public class LanguageBusinessRules
+    {
+        private readonly ILanguageRepository _LanguageRepository;
+
+        public LanguageBusinessRules(ILanguageRepository LanguageRepository)
+        {
+            _LanguageRepository = LanguageRepository;
+        }
+
+        public async Task LanguageNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<Language> result = await _LanguageRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Language name exists.");
+        }
+
+        public void LanguageShouldExistWhenRequested(Language Language)
+        {
+            if (Language == null) throw new BusinessException("Requested Language does not exist.");
+        }
+    }
+}
